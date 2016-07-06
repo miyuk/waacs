e58dd6f6f4b0dd906981fa81b3426c33402cb857
Yuki Miyashita <enough7531@gmail.com>
2016-07-06 14:34:15 +0900

    moving bin directory

---
+++ bin/issuer.py
@@ -0,0 +1,85 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+lib_dir = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(1, lib_dir)
+import waacs
+from waacs import nfcclient, nfcserver, nfcconnection, parameter, tlsclient, stringutils
+import ConfigParser
+import threading
+import time
+import nfc
+import nfc.ndef
+import logging
+logger = logging.getLogger(__name__)
+import json
+
+# issuer.cfgを読み込むためにカレントディレクトリ変更
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+config = ConfigParser.SafeConfigParser()
+config.read("./issuer.cfg")
+ssid = config.get("NfcConnection", "ssid")
+issuer_id = config.get("IssuerAuth", "issuer_id")
+issuer_password = config.get("IssuerAuth", "issuer_password")
+server_address = config.get("TlsClient", "server_address")
+server_port = config.getint("TlsClient", "server_port")
+
+
+def main(argc, argv):
+    log_init()
+    nfc_conn = nfcconnection.NfcConnection()
+    while True:
+        if not nfc_conn.connect():
+            logger.warn("can't connect NFC")
+            continue
+        param = issue_user()
+        json = param.to_json()
+        nfc_conn.send_waacs_message(json)
+        nfc_conn.close()
+        raw_input()
+
+
+def issue_user():
+    # serverに接続
+    # TODO: 失敗してもなにか送る
+    client = tlsclient.TlsClient()
+    client.connect(server_address, server_port)
+    request = dict()
+    request["action"] = "REQUEST_USER"
+    request["issuerId"] = issuer_id
+    request["issuerPassword"] = issuer_password
+    json_text = json.dumps(request)
+    client.write(json_text)
+    data = client.read()
+    response = json.loads(data)
+    # reponseのstatusがOKならパラメータを取得して返す
+    if response["status"] == "OK":
+        user_id = response["userId"]
+        password = response["password"]
+        issuance_time = stringutils.parse_time(response["issuanceTime"])
+        expiration_time = stringutils.parse_time(response["expirationTime"])
+    else:
+        logger.warn("can't login")
+    client.close()
+    param = parameter.Parameter()
+    param.user_id = user_id
+    param.ssid = ssid
+    param.password = password
+    param.issuance_time = issuance_time
+    param.expiration_time = expiration_time
+    return param
+
+
+def log_init():
+    loglevel = logging.DEBUG
+    format = "%(asctime)8s.%(msecs)03d|[%(name)s %(lineno)d(%(levelname)s)] %(message)s"
+    date_format = "%H:%M:%S"
+    logging.basicConfig(level=loglevel,
+                        format=format,
+                        datefmt=date_format)
+
+if __name__ == '__main__':
+    argv = sys.argv
+    main(len(argv), argv)
---
+++ bin/server.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(1, os.path.dirname(sys.path[0]))
+import waacs
+from waacs import userdb, tlslistener, stringutils
+import ConfigParser
+import threading
+import socket
+import logging
+logger = logging.getLogger(__name__)
+import json
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+config = ConfigParser.ConfigParser()
+config.read("./server.cfg")
+listen_address = config.get("TlsServer", "listen_address")
+listen_port = config.getint("TlsServer", "listen_port")
+server_cert = config.get("TlsServer", "server_cert")
+server_key = config.get("TlsServer", "server_key")
+ca_certs = config.get("TlsServer", "ca_certs")
+db_host = config.get("UserDB", "host")
+db_user = config.get("UserDB", "user")
+db_passwd = config.get("UserDB", "password")
+
+
+def main(argc, argv):
+    log_init()
+    db = userdb.UserDB(db_host, db_user, db_passwd)
+    listener = tlslistener.TlsListener(
+        listen_address, listen_port, server_cert, server_key, ca_certs)
+    listener.start()
+    while True:
+        client = listener.accept()
+        # issuerからの取得データはすべてJSON形式
+        data = client.read()
+        request = json.loads(data)
+        # REQUEST_USERならユーザ発行
+        if request["action"] == "REQUEST_USER":
+            issuer_id = request["issuerId"]
+            issuer_password = request["issuerPassword"]
+            res_dict = dict()
+            if not db.check_issuer(issuer_id, issuer_password):
+                res_dict["status"] = "FAIL"
+                client.write(json.dumps(res_dict))
+                client.close()
+                continue
+            user_id, password, issuance_time, expiration_time = issue_user(
+                db, issuer_id)
+            res_dict["status"] = "OK"
+            res_dict["userId"] = user_id
+            res_dict["password"] = password
+            res_dict["issuanceTime"] = stringutils.format_time(issuance_time)
+            res_dict["expirationTime"] = stringutils.format_time(
+                expiration_time)
+            client.write(json.dumps(res_dict))
+            client.close()
+
+
+def issue_user(db, issuer_id):
+        # ランダムなユーザ名とパスワードを作成のみ
+    user_id, password = db.create_user()
+    # データベースに登録する
+    issuance_time, expiration_time = db.issue_user(
+        user_id, password, issuer_id)
+    return (user_id, password, issuance_time, expiration_time)
+
+
+def log_init():
+    loglevel = logging.DEBUG
+    format = "%(asctime)8s.%(msecs)03d|[%(name)s %(lineno)d(%(levelname)s)] %(message)s"
+    date_format = "%H:%M:%S"
+    logging.basicConfig(level=loglevel,
+                        format=format,
+                        datefmt=date_format)
+
+if __name__ == '__main__':
+    argv = sys.argv
+    main(len(argv), argv)
