67b1c6fc4af7cc43fe2c696e3e10010a0172565e
Yuki Miyashita <enough7531@gmail.com>
2016-07-18 16:32:32 +0900

    modified waacs_mod.py

--- radius_modules/waacs_mod.py
+++ radius_modules/waacs_mod.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import radiusd
+import sys
+import os
 import MySQLdb
-
+import radiusd
+from datetime import datetime
 
 HOST = "localhost"
 USER = "waacs"
@@ -23,35 +25,42 @@ def authorize(p):
 def authenticate(p):
     radiusd.radlog(radiusd.L_INFO, "*** python authenticate ***")
     radiusd.radlog(radiusd.L_INFO, str(p))
-    user_id = get_attribute(p, "User-Name").strip('"')
-    password = get_attribute(p, "User-Password").strip('"')
-    sql = "SELECT password FROM {0} WHERE user_id = '{1}'".format(USER_TBL, user_id)
-    result = execute_sql(sql)[0][0]
-    if(result != password):
-        return radiusd.RLM_MODULE_REJECT
+    user_id = get_attribute(p, "User-Name")
+    password = get_attribute(p, "User-Password")
+    mac_addr = get_attribute(p, "Calling-Station-Id")
+    timestamp = get_attribute(p, "Event-Timestamp")
+    timestamp = " ".join(timestamp.split(" ")[:-1])  # タイムゾーン部分を削除
+    timestamp = datetime.strptime(timestamp, "%b %d $Y %H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
+    ap_id = get_attribute(p, "NAS-Identifier")
 
-    return radiusd.RLM_MODULE_OK
+    with MySQLdb.connect(host=HOST, db=DB, user=USER, passwd=PASSWD) as cursor:
+        sql = "SELECT password FROM {0} WHERE user_id = '{1}'".format(USER_TBL, user_id)
+        cursor.execute(sql)
+        result = cursor.fetchall()[0][0]
+        # 登録されていなければReject
+        if(result != password):
+            return radiusd.RLM_MODULE_REJECT
+        sql = "SELECT mac_address FROM {0} WHERE user_id = '{1}'".format(DEVICE_TBL, user_id)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        # 指定台数以上がすでに接続済みでReject
+        if result is not None & & len(result) >= 3:
+            return radiusd.RLM_MODULE_REJECT
+        # 3台以内なら
+        sql = "INSERT INTO {0} (user_id, mac_address, first_access_time, first_access_ap)\
+              VALUES ('{1}', '{2}', '{3}', '{4}')".format(user_id, password, timestamp, ap_id)
+        return radiusd.RLM_MODULE_OK
 
 
 def post_auth(p):
     radiusd.radlog(radiusd.L_INFO, "*** python post_auth ***")
     radiusd.radlog(radiusd.L_INFO, str(p))
-    user_id = get_attribute(p, "User-Name").strip('"')
-    password = get_attribute(p, "User-Password").strip('"')
-    sql = "INSERT"
     return radiusd.RLM_MODULE_OK
-
-
-# backend
-def execute_sql(sql):
-    with MySQLdb.connect(host=HOST, user=USER, passwd=PASSWD, db=DB) as cursor:
-        cursor.execute(sql)
-        return cursor.fetchall()
 
 
 def get_attribute(p, attr_name):
     for attr in p:
         if attr[0] == attr_name:
-            return attr[1]
+            return attr[1].strip('"')
     # if attr_name is nothing
     return ""
