188b1122f0c04d7609413b8e454aefc138cd0889
Yuki Miyashita <enough7531@gmail.com>
2016-08-10 22:52:42 +0900

    importの最適化とsend_waacs_recordにAAR追加

--- waacs/nfc/nfcclient.py
+++ waacs/nfc/nfcclient.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
-import nfc
-import nfc.snep
+from nfc.snep import SnepClient
 import logging
 logger = logging.getLogger(__name__)
 
 
-class NfcClient(nfc.snep.SnepClient):
+class NfcClient(SnepClient):
 
     def __init__(self, llc):
-        nfc.snep.client.SnepClient.__init__(self, llc)
+        SnepClient.__init__(self, llc)
         self.is_sent = False
 
     def send(self, ndef_message):
-        return self._send(ndef_message)
-
-    def _send(self, ndef_message):
         if(not self.put(ndef_message)):
             logger.error("client can't send message")
             return False
