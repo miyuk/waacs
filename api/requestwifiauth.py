586d693c91dffb2a5176fbe9515bdd29e3b25b40
Yuki Miyashita <enough7531@gmail.com>
2016-10-29 23:36:11 +0900

    MySQLdb Ver.1.2.5no

--- api/requestwifiauth.py
+++ api/requestwifiauth.py
@@ -23,19 +23,19 @@ class RequestWifiAuthApi(object):
 
     def on_get(self, req, resp, token):
         with db.connect(**self.db_conn_args) as cur:
-            cur.execute("SELECT COUNT(*) FROM token WHERE token = %s", token)
+            cur.execute("SELECT COUNT(*) FROM token WHERE token = %s", (token, ))
             if not cur.fetchone():
                 logger.warning("not found token: %s", token)
                 resp.status = falcon.HTTP_401
                 return
             while True:
                 user_id = "".join([random.choice(api.SOURCE_CHAR) for x in range(10)])
-                cur.execute("SELECT COUNT(*) FROM user WHERE user_id = %s", user_id)
+                cur.execute("SELECT COUNT(*) FROM user WHERE user_id = %s", (user_id, ))
                 if cur.fetchone()[0] == 0:
                     break
             password = "".joing([random.choice(api.SOURCE_CHAR) for x in range(10)])
             cur.execute("INSERT INTO user(user_id, password, issuance_time) VALUES(%s, %s, %s)",
-                        user_id, password, now.strftime("%Y-%m-%d %H:%M:%S"))
+                        (user_id, password, now.strftime("%Y-%m-%d %H:%M:%S")))
         if "iPhone" in req.user_agent or True:
             resp.status = falcon.HTTP_301
             resp.location = "/download_mobileconfig/{0}/".format(user_id)
