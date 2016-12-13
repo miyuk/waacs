915dfe3485f09fb659e5c3b10cebc4601dd6620f
Yuki Miyashita <enough7531@gmail.com>
2016-12-14 01:37:21 +0900

    QRコードのセンサー発行

--- tokenissuer/qrissuer.py
+++ tokenissuer/qrissuer.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
-
 import logging
 import sqlite3
-import sys
 import time
 import traceback
 from threading import Event, Thread
 
-import qrcode
-from nfc import ContactlessFrontend
-from nfc.ndef import Message, Record, UriRecord
-
+from RPi import GPIO
 from tokenissuer import ApiClient
 
 logger = logging.getLogger(__name__)
@@ -18,31 +13,47 @@ logger = logging.getLogger(__name__)
 
 class QrIssuer(Thread):
 
-    def __init__(self, ssid, api_conf_dict, qr_output_path, update_inteval=30):
+    def __init__(self, ssid, api_conf_dict, qr_conf_dict):
         super(QrIssuer, self).__init__()
         self.ssid = ssid
         self.api_client = ApiClient(api_conf_dict)
-        self.qr_output_path = qr_output_path
-        self.update_inteval = update_inteval
+        self.token_db_file = qr_conf_dict["token_db_file"]
+        self.update_inteval = int(qr_conf_dict["update_interval"])
+        self.sensor_port = int(qr_conf_dict["sensor_port"])
         self.stop_event = Event()
 
     def run(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.sensor_port, GPIO.IN)
         while not self.stop_event.is_set():
             try:
-                token, issuance_time = self.api_client.issue_token(
-                    ApiClient.TYPE_QR)
-                with sqlite3.connect(self.qr_output_path) as cur:
+                while True:
+                    if not self.is_sensing():
+                        break
+                    time.sleep(0.1)
+                token, issuance_time = self.api_client.issue_token(ApiClient.TYPE_QR)
+                with sqlite3.connect(self.token_db_file) as cur:
                     cur.execute("INSERT INTO token(token, issuance_time) \
                         VALUES(?, ?)", (token, issuance_time))
-                # qr_img = qrcode.make(
-                #     self.api_client.requestwifi_url(self.ssid, token))
-                # qr_img.save(self.qr_output_path)
+                while True:
+                    if self.is_sensing():
+                        break
+                    time.sleep(0.1)
             except:
                 logger.error("error: %s", traceback.format_exc())
             finally:
                 self.stop_event.wait(self.update_inteval)
         logger.info("process is stopped")
 
+    def is_sensing(self):
+        try:
+            value = GPIO.input(self.sensor_port)
+            return bool(value)
+        except Exception as e:
+            logger.warning(e.message)
+
     def stop(self):
         logger.debug("process is stopping")
+        GPIO.cleanup()
         self.stop_event.set()
--- waacs-issuer.py
+++ waacs-issuer.py
@@ -1,18 +1,12 @@
 ﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
 import logging
 import os
-import subprocess
 import sys
 import time
 from ConfigParser import SafeConfigParser
 from logging.config import fileConfig
-from threading import Thread
-
-import qrcode
-import requests
 
 from tokenissuer import NfcIssuer, QrIssuer
 
@@ -23,37 +17,25 @@ fileConfig(os.path.join(sys.path[0], "config/issuer_log.cfg"))
 config = SafeConfigParser()
 config.read(os.path.join(sys.path[0], "config/issuer.cfg"))
 qr_conf_dict = dict(config.items("QrIssuer"))
-qr_output_path = qr_conf_dict["qr_output_path"]
-qr_update_interval = int(qr_conf_dict["qr_update_interval"])
 wifi_conf_dict = dict(config.items("WifiInfo"))
 ssid = wifi_conf_dict["ssid"]
 api_conf_dict = dict(config.items("ApiClient"))
-issuer_id = api_conf_dict["issuer_id"]
-issuer_password = api_conf_dict["issuer_password"]
-server_address = api_conf_dict["server_address"]
-server_port = int(api_conf_dict["server_port"])
 
 
 def main(argv):
     nfc_issuer = NfcIssuer(ssid, api_conf_dict)
     nfc_issuer.start()
-    qr_issuer = QrIssuer(ssid, api_conf_dict,
-                         qr_output_path, qr_update_interval)
+    qr_issuer = QrIssuer(ssid, api_conf_dict, qr_conf_dict)
     qr_issuer.start()
-    with open(os.devnull) as devnull:
-        # th = Thread(target=subprocess.call, args=(
-        #     "startx".split(),), kwargs={"stdout": devnull})
-        # th.daemon = True
-        # th.start()
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            logger.warning("exit by KeyboardInterrupt")
-            sys.exit(0)
-        finally:
-            nfc_issuer.stop()
-            qr_issuer.stop()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.warning("exit by KeyboardInterrupt")
+        sys.exit(0)
+    finally:
+        nfc_issuer.stop()
+        qr_issuer.stop()
 
 
 if __name__ == '__main__':
